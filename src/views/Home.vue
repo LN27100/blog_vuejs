46d91156a87f07134beb8ba60ef8d9f2b0b33fe7
LN27100 <heloceinlove@hotmail.fr>
2024-07-25 18:01:31 +0200

    essai deuxième liste échoué

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,30 @@
 <template>
   <div>
-    <h1>Bienvenue sur le blog de Dragon ball Z</h1>
+    <h1>Bienvenue sur le blog de Dragon Ball Z</h1>
+    <h2>Les personnages</h2>
     <ul>
       <li v-for="post in posts" :key="post.id">
         <router-link :to="{ name: 'Post', params: { id: post.id }}">{{ post.title }}</router-link>
         <p>{{ post.summary }}</p>
       </li>
     </ul>
+
   </div>
 </template>
 
 <script>
 export default {
-  props: ['posts']
+  props: {
+    posts: Array,
+  }
 }
 </script>
 
-  
-  <style scoped>
-  /* Styles pour Home.vue */
-
-  h1 {
-color: #2986cc;
-text-align: center;
-font-weight: bold;
-  }
-
-  </style>
-  
+<style scoped>
+/* Styles pour Home.vue */
+h1 {
+  color: #2986cc;
+  text-align: center;
+  font-weight: bold;
+}
+</style>
