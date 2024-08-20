8eb7c31f1c89fabb5086c7f19e6fad5b8b2d88c0
LN27100 <heloceinlove@hotmail.fr>
2024-08-20 12:09:22 +0200

    ajustement titre et ajout boules cristal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -64,14 +64,14 @@ export default {
 <style scoped>
 
 .title-container {
-  text-align: center; 
   margin-top: 2rem;
+  margin-left: 12rem;
 }
 
 .title {
   display: inline-block;
-  letter-spacing: -2px; /* Réduction de l'espacement entre les lettres */
-  font-size: 30px;
+  letter-spacing: -15px;
+  font-size: 25px;
   font-family: 'Chelsea Market', cursive;
   font-weight: bold;
   color: #005a99;
@@ -86,25 +86,25 @@ export default {
 }
 
 /* transformations spécifiques */
-.warp__0 { transform: translate(20.4765px, 96.8363px) rotate(1.041106rad); }
-.warp__1 { transform: translate(29.799999999999997px, 115.91820000000001px) rotate(0.952006rad); }
-.warp__3 { transform: translate(64.5144px, 155.2149px) rotate(0.705882rad); }
-.warp__4 { transform: translate(88.0992px, 167.5382px) rotate(0.600926rad); }
-.warp__5 { transform: translate(98.1284px, 177.9316px) rotate(0.49481rad); }
-.warp__6 { transform: translate(122.4804px, 189.1036px) rotate(0.350878rad); }
-.warp__8 { transform: translate(174.9594px, 200.761px) rotate(0.094828rad); color: #007acc; }
-.warp__9 { transform: translate(202.454px, 201.8496px) rotate(-0.01024rad); color: #007acc; }
-.warp__11 { transform: translate(255.8571px, 196.3169px) rotate(-0.1833rad); color: #007acc; }
-.warp__12 { transform: translate(284.1316px, 191.2629px) rotate(-0.242992rad); color: #007acc; }
-.warp__13 { transform: translate(301.5984px, 185.0673px) rotate(-0.294055rad); color: #007acc; }
-.warp__14 { transform: translate(327.3747px, 176.5083px) rotate(-0.345868rad); color: #53a3d7; }
-.warp__15 { transform: translate(355.8px, 165.9019px) rotate(-0.358303rad); color: #53a3d7; }
-.warp__16 { transform: translate(380.6973px, 157.0402px) rotate(-0.327675rad); color: #53a3d7; }
-.warp__18 { transform: translate(431.6917px, 141.904px) rotate(-0.256447rad); color: #53a3d7; }
-.warp__19 { transform: translate(455.8316px, 135.7603px) rotate(-0.212781rad); color: #53a3d7; }
-.warp__20 { transform: translate(483.0262px, 131.8926px) rotate(-0.174111rad); color: #53a3d7; }
-.warp__21 { transform: translate(495.184px, 129.9196px) rotate(-0.14718rad); color: #53a3d7; }
-.warp__22 { font-size: 45px; color: yellow; transform: translate(530.2073px, 116.13470000000001px) rotate(-0.02699rad); }
+.warp__0 { transform: translate(20px, 90px) rotate(1.041106rad); }
+.warp__1 { transform: translate(30px, 110px) rotate(0.952006rad); }
+.warp__3 { transform: translate(60px, 150px) rotate(0.705882rad); }
+.warp__4 { transform: translate(80px, 168px) rotate(0.600926rad); }
+.warp__5 { transform: translate(90px, 180px) rotate(0.49481rad); }
+.warp__6 { transform: translate(110px, 190px) rotate(0.350878rad); }
+.warp__8 { transform: translate(150px, 195px) rotate(0.094828rad); color: #007acc; }
+.warp__9 { transform: translate(170px, 195px) rotate(-0.01024rad); color: #007acc; }
+.warp__11 { transform: translate(215px, 190px) rotate(-0.1833rad); color: #007acc; }
+.warp__12 { transform: translate(240px, 185px) rotate(-0.242992rad); color: #007acc; }
+.warp__13 { transform: translate(261px, 180px) rotate(-0.294055rad); color: #007acc; }
+.warp__14 { transform: translate(285px, 170px) rotate(-0.345868rad); color: #53a3d7; }
+.warp__15 { transform: translate(305px, 160px) rotate(-0.358303rad); color: #53a3d7; }
+.warp__16 { transform: translate(325px, 150px) rotate(-0.327675rad); color: #53a3d7; }
+.warp__18 { transform: translate(370px, 135px) rotate(-0.256447rad); color: #53a3d7; }
+.warp__19 { transform: translate(392px, 130px) rotate(-0.212781rad); color: #53a3d7; }
+.warp__20 { transform: translate(415px, 125px) rotate(-0.174111rad); color: #53a3d7; }
+.warp__21 { transform: translate(430px, 120px) rotate(-0.14718rad); color: #53a3d7; }
+.warp__22 { font-size: 35px; color: yellow; transform: translate(465px, 110px) rotate(-0.02699rad); }
 
 .cristalBall {
   height: 5rem;
@@ -115,11 +115,12 @@ export default {
 .cristalBall2 {
   height: 5rem;
   width: auto;
-  margin-bottom: 0.5rem; /* Réduction de l'espace entre l'image et le titre */
+  margin-bottom: 0.5rem;
 }
 
 ul {
   margin-top: 10rem;
+  margin-left:2rem;
   padding: 0;
   list-style-type: none;
 }
@@ -138,5 +139,70 @@ a {
 p {
   color: #333333;
   font-weight: bold;
+}
+
+
+
+/* RESPONSIVE */
+@media (max-width: 768px) {
+  .title {
+    font-size: 20px;
+    letter-spacing: -10px;
+  }
+
+  .cristalBall, .cristalBall2 {
+    height: 4rem;
+    margin-left: 0;
+    margin-bottom: 0.25rem;
+  }
+
+  /* Ajustement des transformations pour écrans plus petits */
+  .warp__0 { transform: translate(15px, 75px) rotate(1.041106rad); }
+  .warp__1 { transform: translate(25px, 95px) rotate(0.952006rad); }
+  .warp__3 { transform: translate(40px, 135px) rotate(0.705882rad); }
+  .warp__4 { transform: translate(55px, 155px) rotate(0.600926rad); }
+  .warp__5 { transform: translate(70px, 165px) rotate(0.49481rad); }
+  .warp__6 { transform: translate(90px, 175px) rotate(0.350878rad); }
+  .warp__8 { transform: translate(120px, 180px) rotate(0.094828rad); }
+  .warp__9 { transform: translate(140px, 180px) rotate(-0.01024rad); }
+  .warp__11 { transform: translate(180px, 175px) rotate(-0.1833rad); }
+  .warp__12 { transform: translate(200px, 170px) rotate(-0.242992rad); }
+  .warp__13 { transform: translate(220px, 160px) rotate(-0.294055rad); }
+  .warp__14 { transform: translate(250px, 155px) rotate(-0.345868rad); }
+  .warp__15 { transform: translate(270px, 145px) rotate(-0.358303rad); }
+  .warp__16 { transform: translate(290px, 135px) rotate(-0.327675rad); }
+  .warp__18 { transform: translate(330px, 120px) rotate(-0.256447rad); }
+  .warp__19 { transform: translate(350px, 115px) rotate(-0.212781rad); }
+  .warp__20 { transform: translate(370px, 110px) rotate(-0.174111rad); }
+  .warp__21 { transform: translate(390px, 110px) rotate(-0.14718rad); }
+  .warp__22 { font-size: 25px; transform: translate(420px, 95px) rotate(-0.02699rad); }
+
+  ul {
+    margin-top: 5rem;
+  }
+
+  li {
+    margin-bottom: 0.5rem;
+  }
+}
+
+@media (max-width: 480px) {
+  .title {
+    font-size: 15px;
+    letter-spacing: -12px;
+  }
+
+  .cristalBall, .cristalBall2 {
+    height: 3rem;
+  }
+
+
+  ul {
+    margin-top: 3rem;
+  }
+
+  li {
+    margin-bottom: 0.25rem;
+  }
 }
 </style>
