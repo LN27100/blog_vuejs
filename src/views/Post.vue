1cb63d286078d41f9a5a62f40cd47c228f32f1e3
LN27100 <heloceinlove@hotmail.fr>
2024-08-20 15:21:44 +0200

    ajout bouton retour accueil

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,11 +1,15 @@
 <template>
-  <div v-if="post">
-    <h1>{{ post.title }}</h1>
-    <img v-if="post.image" :src="post.image" :alt="post.title" class="pictures"/>
-    <div class="describ" v-html="post.content"></div>
-  </div>
-  <div v-else>
-    <p>Article non trouvé</p>
+      <button @click="goHome" class="back-button">Accueil</button>
+
+  <div>
+    <div v-if="post">
+      <h1>{{ post.title }}</h1>
+      <img v-if="post.image" :src="post.image" :alt="post.title" class="pictures"/>
+      <div class="describ" v-html="post.content"></div>
+    </div>
+    <div v-else>
+      <p>Article non trouvé</p>
+    </div>
   </div>
 </template>
 
@@ -23,7 +27,6 @@ export default {
     };
   },
   created() {
-    // Vérifiez si les données des posts sont disponibles avant d'essayer de les utiliser
     if (!Array.isArray(this.posts)) {
       console.error('Les données des posts sont manquantes ou mal formatées.', this.posts);
       return;
@@ -34,6 +37,11 @@ export default {
 
     if (!this.post) {
       console.error('Aucun article trouvé avec l\'id:', postId);
+    }
+  },
+  methods: {
+    goHome() {
+      this.$router.push('/'); // Redirige vers la page d'accueil
     }
   }
 }
@@ -46,9 +54,28 @@ export default {
 }
 
 .pictures {
-  display: block; /* Traitement de l'image comme un bloc pour permettre le centrage avec marges */
+  display: block;
   margin: 0 auto;
   width: 20rem;
   height: auto;
+}
+
+.back-button {
+  display: inline-block;
+  padding: 0.5rem 0.5rem;
+  margin-top: 1rem;
+  margin-left: 1rem;
+  background-color: yellow;
+  color: black;
+  border: none;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+}
+
+.back-button:hover {
+  background-color: #005a99;
+  color: white;
 }
 </style>
